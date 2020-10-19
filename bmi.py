10d120d21a3a670664392d1505b1154aad6f2e6f
danielleyo <69905247+danielleyo@users.noreply.github.com>
2020-10-18 23:47:31 -0400

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,8 @@
+import sys
+
+bmiweight = input("Enter your weight: ")
+  # print(type(bmiweight))
+bmiheighgt = input("Enter you height: ")
+  # print(type(bmiheight))
+  bmi = (float(bmiheight) * (float(bmiweight)
+  print("This is your bmi" + str(bmi)
