30f9d3ae62d87e75029b058922aab437bcaa3e0f
danielleyo <69905247+danielleyo@users.noreply.github.com>
2020-10-18 23:42:28 -0400

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,6 @@
+import sys
+
+tempincelsius = input("Please enter a temperature in celsius: ")
+  print(type(tempincelsius))
+  # tempinfahrenheit = (tempincelsius * (9 / 5)) + 32.0
+  # print("The temperature is " + tempinfahrenheit + "°F") 
